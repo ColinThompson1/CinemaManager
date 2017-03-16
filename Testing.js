ca37203c132a529a39157d46ceed4fa0f16153fe
Colin Thompson <colinthompson@Colins-MacBook-Pro.local>
2017-03-16 17:36:36 -0600

    Messing around with the http server

--- Testing.js
+++ Testing.js
@@ -5,11 +5,15 @@
 // Import Modules
 var http = require("http");
 
+const HOSTNAME = "127.0.0.1";
+const PORT = 8081 //For simplicity for now. Need root access for 80.
+
+
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type' : 'text/plain'});
     response.end('Hello World\n')
 
 
-}).listen(8081);
+}).listen(PORT);
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at http://" + HOSTNAME + ":" + PORT);
