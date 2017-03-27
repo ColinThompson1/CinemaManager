b57d0984d3b0758f9ee7fc8061346b521976faa6
Colin Thompson <colinthompson@Colins-Mac-Pro.local>
2017-03-27 00:10:10 -0600

    Finished Login/Register UI

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,28 @@
 /**
  * Created by colinthompson on 2017-03-25.
  */
+$('#login-form-link').click(function(e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+});
+$('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+});
+
+$('#bday').datepicker({
+    format: 'mm/dd/yyyy',
+    showOtherMonths: true,
+    selectOtherMonths: true,
+    autoclose: true,
+    changeMonth: true,
+    changeYear: true,
+    orientation: "bottom"
+});
+
