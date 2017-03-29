7bc92cc1529b0dee3ec7d86be10130589a113dd5
Colin Thompson <colinthompson@Colins-MacBook-Pro.local>
2017-03-29 16:30:25 -0600

    Basic Post Implementation

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
  */
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const PORT = 8081; //For simplicity, since you need root to make it on port 80
 const HOSTNAME = "127.0.0.1";
@@ -10,6 +14,16 @@ const HOSTNAME = "127.0.0.1";
 app.use(express.static(__dirname +'/public')); //Serves static files to client
 
 app.get('/', function (req, res) { //Callback for main page
+    res.sendFile( __dirname + "/public/views/index.html");
+});
+
+app.post('/userlogin', urlencodedParser, function (req, res) {
+    var employeeLogin = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    console.log(employeeLogin);
     res.sendFile( __dirname + "/public/views/index.html");
 });
 
