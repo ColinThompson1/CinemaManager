748591228e00d7fe42ee58870952022a4fa1ad90
Colin Thompson <colinthompson@Colins-Mac-Pro.local>
2017-03-31 00:51:54 -0600

    EJS Templating Framework Integration
    Added EJS
    Updated index.html to be more modular by using ejs

--- app/routes.js
+++ app/routes.js
@@ -9,18 +9,19 @@ var path = require('path');
 module.exports = function (app, passport) {
 
     app.get('/', function (req, res) { //Callback for main page
-        res.sendFile("index.html", {root: path.join(__dirname, "../public/views/")});
+        // res.render("index.html", {root: path.join(__dirname, "../public/views/")});
+        res.render("index.html");
     });
 
-    // //Handler for User Login
-    // app.post('/userlogin', urlencodedParser, function (req, res) {
-    //     var employeeLogin = { //Credentials
-    //         email: req.body.email,
-    //         password: req.body.password
-    //     };
-    //
-    //     console.log(employeeLogin);
-    //     res.sendFile( __dirname + "/public/views/index.html");
-    // });
+    //Handler for User Login
+    app.post('/userlogin', function (req, res) {
+        var employeeLogin = { //Credentials
+            email: req.body.email,
+            password: req.body.password
+        };
+
+        console.log(employeeLogin);
+        res.send("logged in");
+    });
 
 };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
  * Created by colinthompson on 2017-03-22.
  */
 
+'use strict';
+
 // Set Up =============================================
 var express = require('express');
 var session = require('express-session');
@@ -10,12 +12,19 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var flash = require('connect-flash');
 var morgan = require('morgan');
+var ejs = require('ejs');
 var app = express();
 
 const PORT = 8081; //For simplicity, since you need root to make it on port 80
 const HOSTNAME = "127.0.0.1";
 
 // Config =============================================
+
+app.set('view engine', 'html'); //Use the template module to render html
+app.engine('html', ejs.renderFile);
+app.set('views', __dirname  + "/public/views/pages"); //Instruct the engine of the location of the views
+
+
 
 app.use(morgan('dev')); //Logs to the console
 app.use(cookieParser()); //Cookies for Auth
