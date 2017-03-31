8b3186df54cbb0a744ada77253976c2df75a49f7
Mark <markgllin@hotmail.com>
2017-03-30 19:52:08 -0600

    snippets connecting  client to db

---
+++ database/queries.sql
@@ -0,0 +1,101 @@
+﻿#CUSTOMER ####################################################
+
+#store CUSTOMER info
+INSERT
+INTO CUSTOMERS (ID, FNAME, LNAME, BDATE, SEX, ADDRESS, CREDIT_CARD_NO, EMAIL, PHONE_NO)
+VALUES (<respective values>)
+
+#update customer info
+
+
+#check/retrieve customer
+#delete customer info
+
+#MOVIE #######################################################
+
+#store movie information
+INSERT
+INTO MOVIE (ID, TITLE, RELEASE_DATE, LENGTH, EARNINGS, AIR_LENGTH, POSTER_PATH)
+VALUES (<respective values>)
+
+#retrieve movie info
+#update movie info
+#delete movie info
+
+#CONCESSIONS #################################################
+
+#store concessions info
+INSERT
+INTO CONCESSIONS(SKU, ITEM, PRICE, BARCODE)
+VALUES (<respective values>)
+
+#retrieve concessions info
+#update concessions info
+#delete concessions info
+
+#ADVERTISERS #################################################
+
+#store adverstiser info
+INSERT
+INTO ADVERTISERS (ID, AD_COMPANY, REP_NAME, REP_EMAIL, REP_PHONE_NO)
+VALUES (<respective values>)
+
+#retrieve concessions info
+#update concessions info
+#delete concessions info
+
+#AUDITORIUM ##################################################
+
+#store auditorium info
+INSERT
+INTO AUDITORIUM (AUD_NO, SEAT_COUNT, LOCATION, SHOWING_ID)
+VALUES (<respective values>)
+
+#retrieve concessions info
+#update concessions info
+#delete concessions info
+
+#DEPARTMENT ##################################################
+
+#store department info
+INSERT
+INTO DEPARTMENT (DNO, MNGR_SSN, DNAME)
+VALUES (<respective values>)
+
+#retrieve concessions info
+#update concessions info
+#delete concessions info
+
+#EMPLOYEE ####################################################
+
+#store employee info
+INSERT
+INTO EMPLOYEE (SSN, FNAME, LNAME, BDATE, DNO, SUPERSSN, SEX, ADDRESS, PHONE_NO, SALARY, EMAIL)
+VALUES (<respective values>)
+
+#retrieve concessions info
+#update concessions info
+#delete concessions info
+
+#SHOWING #####################################################
+
+#store showing info
+INSERT
+INTO SHOWING (ID, START_TIME, PRICE, MOVIE_ID)
+VALUES (<respective values>)
+
+#retrieve showing info
+#update showing info
+#delete showing info
+
+
+#TICKETS #####################################################
+
+#store ticket info
+INSERT
+INTO TICKETS (ID, SEAT_NO, SHOWING_ID, CUSTOMER_ID)
+VALUES (<respective values>)
+
+#retrieve ticket info
+#update ticket info
+#delete ticket info
--- server.js
+++ server.js
@@ -4,6 +4,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    server: "localhost",
+    database: "cinema",
+    user: "root",
+    password: "marklin",
+    port: 3306
+});
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -25,6 +34,9 @@ app.post('/userlogin', urlencodedParser, function (req, res) {
 
     console.log(employeeLogin);
     res.sendFile( __dirname + "/public/views/index.html");
+
+    //AUTHENTICATE wITH DB
+
 });
 
 var server = app.listen(PORT, HOSTNAME, function () {
@@ -33,3 +45,23 @@ var server = app.listen(PORT, HOSTNAME, function () {
     console.log("Example app listening at http://%s:%s", host, port)
 });
 
+connection.connect(function(err) {
+    if (err){
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+});
+
+connection.query({
+    sql: 'SELECT FNAME FROM EMPLOYEE'
+},
+    function (error, results, fields) {
+        // error will be an Error if one occurred during the query
+        // results will contain the results of the query
+        // fields will contain information about the returned results fields (if any)
+
+        console.log(results);
+    }
+);
